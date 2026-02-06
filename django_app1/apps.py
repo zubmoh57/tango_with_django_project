976586a091f1a371e68080019002cfb1f5e290f3
“Zubair <3014899m@student.gla.ac.uk>
2026-02-06 17:08:10 +0000

    Completed: Chapter 3

---
+++ django_app1/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DjangoApp1Config(AppConfig):
+    name = 'django_app1'
---
+++ rango/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from rango import views
+
+app_name = 'rango'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+]
+
+
+
+
+
+# page 46 / 438 (should prob commit these changes)
