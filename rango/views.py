43e41e0bf907468e50c5d0ed41fdcaa4a6161d0c
“Zubair <3014899m@student.gla.ac.uk>
2026-02-11 18:38:40 +0000

    Completed: Chapter 8

--- rango/views.py
+++ rango/views.py
@@ -15,7 +15,10 @@ def index(request):
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return render(request, 'rango/about.html')
+    print(request.method)
+    print(request.user)
+    
+    return render(request, 'rango/about.html', {})
 
 def show_category(request, category_name_slug):
     context_dict = {}
